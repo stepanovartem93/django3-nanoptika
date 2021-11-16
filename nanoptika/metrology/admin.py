8b8bc54fd5534fcb30e30d0b45700eff2ef0844d
Stepanov Artem <artemis6218@yandex.ru>
2021-11-16 08:25:04 +0300

    -добавил модель средств СИ; -зарегистрировал модель в админ панели.

--- nanoptika/metrology/admin.py
+++ nanoptika/metrology/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import TypeOfMeasurment, MeasuringInstrument
 
-# Register your models here.
+@admin.register(MeasuringInstrument)
+class MeasuringInstrumentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type_of_measurment', 'state_register_number', 
+    'factory_number', 'expluatation_place', 'status',)
+    list_filter = ('type_of_measurment', 'state_register_number', 'expluatation_place', 'status',)
+    search_fileds = ('name','state_register_number',)
+    ordering = ('state_register_number', 'status', 'expluatation_place',)
+
+
+@admin.register(TypeOfMeasurment)
+class TypeOfMeasurmentAdmin(admin.ModelAdmin):
+    list_display = ('measurment',)
--- nanoptika/metrology/models.py
+++ nanoptika/metrology/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class TypeOfMeasurment(models.Model):
+    measurment = models.CharField(verbose_name='Вид измерения', max_length=64, unique=True)
+
+    def __str__(self):
+        return f'{self.measurment}'
+
+class MeasuringInstrument(models.Model):
+    STATUS_CHOICES = (
+        ('in repair', 'В ремонте'),
+        ('in work', 'В работе'),
+        ('in verification', 'В поверке'),
+        ('in calibration', 'На калибровке'),
+        ('in storage', 'На хранении'),
+    )
+    name = models.CharField(verbose_name='Наименование СИ', max_length=128)
+    type_of_measurment = models.ForeignKey(TypeOfMeasurment, verbose_name='Тип измерений', on_delete=models.CASCADE)
+    state_register_number = models.CharField(verbose_name='ГРСИ №', max_length=16)
+    # year = models.CharField(verbose_name='Год выпуска', max_length=4, default=0)
+    # invertarian_number = models.CharField(verbose_name='Инвертарный номер', max_length=6, default=0000)
+    factory_number = models.CharField(verbose_name='Заводской номер', max_length=16)
+    expluatation_place = models.CharField(verbose_name='Место эксплуатации', max_length=128)
+    status = models.CharField(max_length=16, choices=STATUS_CHOICES, default='in_storage')
+
+    def __str__(self):
+        return f'{self.name}'
